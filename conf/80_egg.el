0cef6f64f120b451dcf420f56f9d677627111d40
tomi <tomiacannondale@gmail.com>
2013-06-09 01:35:51 +0900

    Delete 80_egg.el

--- conf/80_egg.el
+++
@@ -1,7 +0,0 @@
-;;========================================================
-;; egg設定
-;; git clone https://github.com/byplayer/egg.git egg-mode
-;;========================================================
-(when (executable-find "git")
-  (require 'egg nil t))
-(define-key egg-status-buffer-mode-map "A" 'egg-stage-untracked-files)

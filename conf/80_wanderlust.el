fc637ea26d2b1f29402d9f0cef807a46147cc54f
tomi <tomiacannondale@gmail.com>
2013-06-09 01:12:08 +0900

    Require mime-w3m instead of loading mime-setup in 80_wanderlust.el
    To read html mail.

--- conf/80_wanderlust.el
+++ conf/80_wanderlust.el
@@ -1,8 +1,8 @@
 ;;====================================================
 ;; wanderlust設定
 ;;====================================================
-(load "mime-setup")
 (setq w3m-command "/usr/local/bin/w3m")
+(require 'mime-w3m)
 (autoload 'wl "wl" "Wanderlust" t)
 (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
 (autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)

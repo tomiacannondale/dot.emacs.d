380638d5f1c2a062c4d2afa6db08d27318930441
tomi <tomiacannondale@gmail.com>
2013-06-09 01:12:44 +0900

    Install wanderlust by el-get

--- conf/80_wanderlust.el
+++ conf/80_wanderlust.el
@@ -7,4 +7,4 @@
 (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
 (autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
 
-(setq wl-icon-directory (expand-file-name "~/.emacs.d/etc/wl" ))
+(setq wl-icon-directory (expand-file-name "~/.emacs.d/el-get/wanderlust-info-ja/icons"))

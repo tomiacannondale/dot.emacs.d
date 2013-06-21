716ba8fef5b2cfece9a8fd2942ea22270feae6c8
tomi <tomiacannondale@gmail.com>
2013-06-21 19:53:56 +0900

    Delete auto-install

--- conf/20_auto-install.el
+++
@@ -1,6 +0,0 @@
-;===================================================================
-; auto-install設定
-;===================================================================
-(require 'auto-install)
-(setq auto-install-directory "~/.emacs.d/auto-install/")
-(auto-install-compatibility-setup)

1d651d33b4dc157b03ed6df8dcf41743be7381e5
tomi <tomiacannondale@gmail.com>
2013-06-21 19:54:21 +0900

    Delete install-elisp

--- conf/20_install-elisp.el
+++
@@ -1,7 +0,0 @@
-;;==================================================================
-;; install-elisp設定
-;; cd ~/.emacs.d/elisp
-;; wget http://www.emacswiki.org/cgi-bin/wiki/download/install-elisp.el
-;;==================================================================
-(require 'install-elisp)
-(setq install-elisp-repository-directory "~/.emacs.d/elisp/")

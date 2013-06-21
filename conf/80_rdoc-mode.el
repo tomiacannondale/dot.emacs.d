4da9798b3d30b0904195243e2047c61da6c6b5bc
tomi <tomiacannondale@gmail.com>
2013-06-21 19:54:51 +0900

    Delete rdoc-mode

--- conf/80_rdoc-mode.el
+++
@@ -1,7 +0,0 @@
-;;========================================================
-;; rdoc-mode
-;; ruby-modeに同封
-;;========================================================
-(load "rdoc-mode")
-(autoload 'rdoc-mode "RDoc")
-(add-to-list 'auto-mode-alist '("\\.rdoc$" . rdoc-mode))

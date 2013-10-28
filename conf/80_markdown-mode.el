601c175247247e6d5f02e4b9b35805281faf9ddc
tomi <tomiacannondale@gmail.com>
2013-10-29 01:03:14 +0900

    Use add-to-list insted of setq
    setq version can not use gfm mode on ubuntu

--- conf/80_markdown-mode.el
+++ conf/80_markdown-mode.el
@@ -5,5 +5,5 @@
 ;;========================================================
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-   (cons '("\\.md" . gfm-mode) auto-mode-alist))
+(add-to-list 'auto-mode-alist
+	     '("\\.md" . gfm-mode))

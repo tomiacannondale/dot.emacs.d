c569e9299fe7c96ed00d87d4d0dd215ac27d1fb4
tomi <tomiacannondale@gmail.com>
2013-12-22 02:28:30 +0900

    Change yas-snippets-dirs

--- conf/60_yasnippet.el
+++ conf/60_yasnippet.el
@@ -5,3 +5,7 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 (setq yas-prompt-functions '(yas-dropdown-prompt yas-x-prompt yas-completing-prompt yas-ido-prompt yas-no-prompt))
+
+; snippet置き場を変更(自分で追加するsnippetは最初のディレクトリになる
+(setq yas-snippet-dirs
+      '("~/.emacs.d/etc/snippets" "/Users/tomi/.emacs.d/el-get/yasnippet/snippets"))

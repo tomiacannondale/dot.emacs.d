7936a18a1c2b68b454f414fa81b46482610cd81a
tomi <tomiacannondale@gmail.com>
2014-01-06 19:02:44 +0900

    Delete to set magit-emacsclient-excutable
    New version emacs is unnecessary.

--- conf/80_magit.el
+++ conf/80_magit.el
@@ -12,9 +12,5 @@
 ;; `M-x magit-status` で選択出来るディレクトリ
 (setq magit-repo-dirs '("~/.emacs.d" "~/.zsh.d" "~/code"))
 
-;; 使用するemacsclientを現在のemacsと同じディレクトリにあるemacsclientにする。
-;; なぜか /usr/bin/emacsclient を呼んでしまう
-(setq magit-emacsclient-executable "/usr/local/bin/emacsclient")
-
 ;; magit-status
 (define-key global-map "\C-xvd" 'magit-status)

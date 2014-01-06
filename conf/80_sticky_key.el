9e36b6609f38447753cbbfb8fd05b218d7ae1c3b
tomi <tomiacannondale@gmail.com>
2014-01-06 18:59:51 +0900

    Add sticky_key.el

---
+++ conf/80_sticky_key.el
@@ -0,0 +1,25 @@
+;; http://homepage1.nifty.com/blankspace/emacs/sticky.html
+;; 大文字(shiftの入力)を `;` で代替する
+;; `;` そのものを入れたいときは `;` を連続で入力する。
+(defvar sticky-key ";")
+(defvar sticky-list
+  '(("a" . "A")("b" . "B")("c" . "C")("d" . "D")("e" . "E")("f" . "F")("g" . "G")
+    ("h" . "H")("i" . "I")("j" . "J")("k" . "K")("l" . "L")("m" . "M")("n" . "N")
+    ("o" . "O")("p" . "P")("q" . "Q")("r" . "R")("s" . "S")("t" . "T")("u" . "U")
+    ("v" . "V")("w" . "W")("x" . "X")("y" . "Y")("z" . "Z")
+    ("1" . "!")("2" . "@")("3" . "#")("4" . "$")("5" . "%")("6" . "^")("7" . "&")
+    ("8" . "*")("9" . "(")("0" . ")")
+    ("`" . "~")("[" . "{")("]" . "}")("-" . "_")("=" . "+")("," . "<")("." . ">")
+    ("/" . "?")(";" . ":")("'" . "\"")("\\" . "|")
+    ))
+(defvar sticky-map (make-sparse-keymap))
+(define-key global-map sticky-key sticky-map)
+(mapcar (lambda (pair)
+          (define-key sticky-map (car pair)
+            `(lambda()(interactive)
+               (setq unread-command-events
+                     (cons ,(string-to-char (cdr pair)) unread-command-events)))))
+        sticky-list)
+(define-key sticky-map sticky-key '(lambda ()(interactive)(insert sticky-key)))
+
+(define-key isearch-mode-map sticky-key sticky-map)

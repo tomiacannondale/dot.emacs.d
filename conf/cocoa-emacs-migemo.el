d42eea1c2ae9aee3a5e18948ab8240a0ae4acb1a
tomi <tomiacannondale@gmail.com>
2014-01-09 00:01:42 +0900

    Apply migemo for marvericks
    * Fix hang up migemo isearch
      http://d.hatena.ne.jp/higepon/20060626/1151327464

--- conf/cocoa-emacs-migemo.el
+++ conf/cocoa-emacs-migemo.el
@@ -4,20 +4,19 @@
 ;; http://0xcc.net/migemo/  (migemoホーム)
 ;; https://gist.github.com/457761 (migemo.el公開gist)
 ;;========================================================
-(load "migemo")
-(setq migemo-command "cmigemo")
-(setq migemo-options '("-q" "--emacs" "-i" "\a"))
-(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(require 'migemo)
+(setq migemo-command "/opt/boxen/homebrew/bin/cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+(setq migemo-dictionary "/opt/boxen/homebrew/share/migemo/utf-8/migemo-dict")
 (setq migemo-coding-system 'utf-8-unix)
 (setq migemo-user-dictionary nil)
 (setq migemo-regex-dictionary nil)
+(load-library "migemo")
+(migemo-init)
+(setq migemo-accept-process-output-timeout-msec 80)
 
-;;;========================================================
-;;; moccurでmigemoを使う
-;;; http://www.bookshelf.jp/soft/meadow_50.html
-;;;========================================================
+;; ========================================================
+;; moccurでmigemoを使う
+;; http://www.bookshelf.jp/soft/meadow_50.html
+;; ========================================================
 (setq moccur-use-migemo t)
-
-;; Emacs24.3でカーソルが移動しない対処
-;; http://qiita.com/items/bda92200f4a48ca89228
-(setq search-whitespace-regexp nil)

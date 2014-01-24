9dd7f5ef2ad32c26409a2798fdb4d2f6b5d189df
tomi <tomiacannondale@gmail.com>
2014-01-25 03:37:04 +0900

    Delete setting of w3m-command
    Perhaps, is no longer needed be to use exec-path-from-shell.

--- conf/80_wanderlust.el
+++ conf/80_wanderlust.el
@@ -1,7 +1,6 @@
 ;;====================================================
 ;; wanderlust設定
 ;;====================================================
-(setq w3m-command "/usr/local/bin/w3m")
 (require 'mime-w3m)
 (autoload 'wl "wl" "Wanderlust" t)
 (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)

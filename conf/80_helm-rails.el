8a23e9b85e9cb59417e62d95586b24ca635adb04
tomi <tomiacannondale@gmail.com>
2014-02-24 01:01:41 +0900

    Add helm-rails-factories

--- conf/80_helm-rails.el
+++ conf/80_helm-rails.el
@@ -2,3 +2,4 @@
 
 ;; turnipのspecを追加する
 (helm-rails-def-resource 'acceptances "spec/acceptance/" "^spec/acceptance/(.+)$")
+(helm-rails-def-resource 'factories "spec/factories/" "^spec/factoris/(.+.rb)$")

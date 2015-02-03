0491e2cbbf39289026932692d047dd9db9051ec1
tomi <tomiacannondale@gmail.com>
2015-02-04 00:58:00 +0900

    Add ac-source-yasnippet to ac-ruby-mode-setup

--- conf/60_auto-complete.el
+++ conf/60_auto-complete.el
@@ -7,3 +7,8 @@
 (global-auto-complete-mode t)
 (define-key ac-complete-mode-map "\C-n" 'ac-next)
 (define-key ac-complete-mode-map "\C-p" 'ac-previous)
+
+(defun ac-ruby-mode-setup ()
+  (setq-default ac-sources '(ac-source-yasnippet)))
+
+(add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)

87a6b417c498c452a9329512f440405d2dad0144
tomi <tomiacannondale@gmail.com>
2015-02-23 00:54:39 +0900

    Apply to turnip feature-xx-verify

--- conf/80_feature-mode.el
+++ conf/80_feature-mode.el
@@ -4,3 +4,16 @@
 ;;========================================================
 (require 'feature-mode)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+
+(defun my-feature-single-veriry ()
+  (interactive)
+  (rspec-compile
+   (concat (buffer-file-name) ":" (number-to-string (line-number-at-pos)))
+   (rspec-core-options)))
+
+(defun my-feature-veriry ()
+  (interactive)
+  (rspec-compile (buffer-file-name) (rspec-core-options)))
+
+(define-key feature-mode-map (kbd "\C-c,v") 'my-feature-veriry)
+(define-key feature-mode-map (kbd "\C-c,s") 'my-feature-single-veriry)

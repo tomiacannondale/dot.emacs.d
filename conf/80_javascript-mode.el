e8761488a00fd8a908803da59476f760e9d0135a
tomi <tomiacannondale@gmail.com>
2015-04-12 03:48:42 +0900

    Delete setting of javascript-mode
    Use js2-mode

--- conf/80_javascript-mode.el
+++
@@ -1,6 +0,0 @@
-;;========================================================
-;; javascript-mode設定
-;; auto-install from url RET http://www.karllandstrom.se/downloads/emacs/javascript.el
-;;========================================================
-(add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
-(autoload 'javascript-mode "javascript" nil t)

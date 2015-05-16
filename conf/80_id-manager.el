a263a3aa1a9b3e4c6634b6a5869f819c67911953
tomi <tomiacannondale@gmail.com>
2015-05-17 03:32:15 +0900

    Delete forked id-manager
    Enable heml on id-manager

--- conf/80_id-manager.el
+++ conf/80_id-manager.el
@@ -1,9 +1,3 @@
-;; helmを使えるように独自に改造
-(setq load-path (cons "~/code/vendor/emacs-id-manager/" load-path))
-
-;; インターフェースはhelmを使う(require前に書かないとうまく動かない)
-(setq idm-anything-or-helm "helm")
-
 (require 'id-manager)
 
 ;; ファイルの置き場所

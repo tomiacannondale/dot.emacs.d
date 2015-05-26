565fbf33b24a84ef0ecb970bce12aab3172a8d69
tomi <tomiacannondale@gmail.com>
2015-05-27 01:36:16 +0900

    Modify indent for js2-mode

--- conf/80_js2-mode.el
+++ conf/80_js2-mode.el
@@ -1,1 +1,9 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;; インデントを調整
+;; http://d.hatena.ne.jp/hiratara/20090211/1234335014
+(add-hook 'js2-mode-hook
+          '(lambda ()
+             (setq js2-basic-offset 2
+                   tab-width        2
+                   js2-cleanup-whitespace nil)))

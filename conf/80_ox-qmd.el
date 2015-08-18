7158f2777b404f3bdd461e5423fd3c980e7ab455
tomi <tomiacannondale@gmail.com>
2015-08-19 01:55:03 +0900

    Add mx-qmd

---
+++ conf/80_ox-qmd.el
@@ -0,0 +1,4 @@
+(require 'ox-qmd)
+(add-to-list 'ox-qmd-language-keyword-alist '("ruby" . "ruby"))
+(add-to-list 'ox-qmd-language-keyword-alist '("js" . "js"))
+(add-to-list 'ox-qmd-language-keyword-alist '("sh" . "sh"))

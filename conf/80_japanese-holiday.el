070937db459764d6f9d86b3ba6ce4654d704ea39
tomi <tomiacannondale@gmail.com>
2015-09-28 00:58:41 +0900

    Change setting for japanese-holidays

--- conf/80_japanese-holiday.el
+++ conf/80_japanese-holiday.el
@@ -6,15 +6,15 @@
   '(progn
      (require 'japanese-holidays)
      (setq calendar-holidays ; 他の国の祝日も表示させたい場合は適当に調整
-           (append japanese-holidays local-holidays other-holidays))
+           (append japanese-holidays holiday-local-holidays holiday-other-holidays))
      (setq mark-holidays-in-calendar t) ; 祝日をカレンダーに表示
      ;; 土曜日・日曜日を祝日として表示する場合、以下の設定を追加します。
-     ;; デフォルトで設定済み
+     ;; 変数はデフォルトで設定済み
      (setq japanese-holiday-weekend '(0 6)     ; 土日を祝日として表示
            japanese-holiday-weekend-marker     ; 土曜日を水色で表示
            '(holiday nil nil nil nil nil japanese-holiday-saturday))
      (add-hook 'calendar-today-visible-hook 'japanese-holiday-mark-weekend)
-     (add-hook 'calendar-today-invisible-hook 'japanese-holiday-mark-weekend)))
-
-;; “きょう”をマークするには以下の設定を追加します。
-(add-hook 'calendar-today-visible-hook 'calendar-mark-today)
+     (add-hook 'calendar-today-invisible-hook 'japanese-holiday-mark-weekend)
+     ;; “きょう”をマークするには以下の設定を追加します。
+     (add-hook 'calendar-today-visible-hook 'calendar-mark-today)))
+(require 'holidays)

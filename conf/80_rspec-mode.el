cca946039860f4ee0a0f8101f3ad51993aa2359b
tomi <tomiacannondale@gmail.com>
2015-09-28 01:02:46 +0900

    Use bin/spring instead spring on rspec-mode

--- conf/80_rspec-mode.el
+++ conf/80_rspec-mode.el
@@ -5,3 +5,14 @@
 (require 'rspec-mode)
 
 (setq rspec-use-rake-when-possible nil)
+
+;; springコマンドをbin/springにする
+(defun rspec-runner ()
+  "Returns command line to run rspec"
+  (let ((bundle-command (if (rspec-bundle-p) "bundle exec " ""))
+        (zeus-command (if (rspec-zeus-p) "zeus " nil))
+        (spring-command (if (rspec-spring-p) "bin/spring " nil)))
+    (concat (or zeus-command spring-command bundle-command)
+            (if (rspec-rake-p)
+                (concat rspec-rake-command " spec")
+              rspec-spec-command))))

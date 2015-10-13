ba89c89c18e66ee6a87a84b5e3a50e69f6c5aff8
tomi <tomiacannondale@gmail.com>
2015-10-14 01:39:56 +0900

    Delete diff face on magit
    diffのfaceが細かくなりすぎて諦めた

--- conf/80_magit.el
+++ conf/80_magit.el
@@ -1,14 +1,5 @@
 (require 'magit)
 
-;; diffの色を変更する
-;; http://qiita.com/nishikawasasaki/items/f690ee08f6a32d9d03fa
-;; 追加した部分を緑に
-(set-face-foreground 'magit-diff-add "green")
-;; 削除した 部分を赤に
-(set-face-foreground 'magit-diff-del "red")
-;; 選択項目ハイライトがうっとうしいので背景色と同化
-(set-face-background 'magit-item-highlight "#000000")
-
 ;; `M-x magit-status` で選択出来るディレクトリ
 (setq magit-repository-directories '("~/.emacs.d" "~/.zsh.d" "~/code"))
 

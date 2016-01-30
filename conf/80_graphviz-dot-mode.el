9dd0551443c4ce9fce872a5251d5125a5f66efcc
tomi <tomiacannondale@gmail.com>
2016-01-30 18:20:27 +0900

    Set graphviz-dot-auto-indent-on-semi to nil
    `;` not add indent

--- conf/80_graphviz-dot-mode.el
+++ conf/80_graphviz-dot-mode.el
@@ -4,4 +4,5 @@
 ;;  http://d.hatena.ne.jp/n9d/20080419/1208614482
 ;;  http://users.skynet.be/ppareit/projects/graphviz-dot-mode/graphviz-dot-mode.html
 ;;========================================================
-(load "graphviz-dot-mode.el")
+(require 'graphviz-dot-mode)
+(setq graphviz-dot-auto-indent-on-semi nil)

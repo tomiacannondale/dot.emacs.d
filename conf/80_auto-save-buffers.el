ab2c561c12c471e9deae1a21f3f1067aecf81d35
tomi <tomiacannondale@gmail.com>
2016-05-03 02:39:17 +0900

    Not auto-save COMMIT_EDITMSG
    This file is used by magit

--- conf/80_auto-save-buffers.el
+++ conf/80_auto-save-buffers.el
@@ -1,3 +1,3 @@
 (require 'auto-save-buffers)
-(setq auto-save-buffers-exclude-regexp "gpg$\\|name.txt$")
+(setq auto-save-buffers-exclude-regexp "gpg$\\|name.txt$\\|COMMIT_EDITMSG$")
 (run-with-idle-timer 0.5 t 'auto-save-buffers)

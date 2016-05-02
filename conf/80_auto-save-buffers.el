c253c209a99ae675c9e85355d361a25865a39897
tomi <tomiacannondale@gmail.com>
2016-05-03 01:56:47 +0900

    Change file name which is disable auto-save-buffers
    idm-db is not match.

--- conf/80_auto-save-buffers.el
+++ conf/80_auto-save-buffers.el
@@ -1,3 +1,3 @@
 (require 'auto-save-buffers)
-(setq auto-save-buffers-exclude-regexp "idm-db$\\|name.txt$")
+(setq auto-save-buffers-exclude-regexp "gpg$\\|name.txt$")
 (run-with-idle-timer 0.5 t 'auto-save-buffers)

e0354f7a080413cac6971b094a1c3a8e4b90c4b0
tomi <tomiacannondale@gmail.com>
2016-06-14 00:23:49 +0900

    Delete electric-spacing
    Fix leak b722e5d292e9f2dcd7759129f8c0fc326f1e4fbc

--- conf/80_electric-spacing.el
+++
@@ -1,8 +0,0 @@
-(require 'electric-spacing)
-(add-to-list 'electric-spacing-regexp-pairs
-             '("{\\|}" . "[0-9A-Za-z]"))
-(add-to-list 'electric-spacing-regexp-pairs
-             '("[0-9A-Za-z]" . "{\\|}"))
-(make-local-variable 'electric-spacing-regexp-pairs)
-
-(add-hook 'ruby-mode-hook 'electric-spacing-mode)

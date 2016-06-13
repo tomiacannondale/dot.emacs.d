7d07a3187ed6180b29b5588932ebce8e19ec14e4
tomi <tomiacannondale@gmail.com>
2016-06-14 00:12:02 +0900

    Change setting of google-translate
    http://blog.shibayu36.org/entry/2016/05/29/123342

--- conf/80_google-translate.el
+++ conf/80_google-translate.el
@@ -1,8 +1,30 @@
-(global-set-key "\C-ct" 'my-google-translate)
+;;; http://blog.shibayu36.org/entry/2016/05/29/123342
+(require 'google-translate)
+(require 'google-translate-default-ui)
 
-(defun my-google-translate (arg)
-  "Translate with google translate."
-  (interactive "p")
-  (if (equal arg 4)
-      (call-interactively 'google-translate-query-translate)
-    (call-interactively 'google-translate-at-point)))
+(defvar google-translate-english-chars "[:ascii:]"
+  "これらの文字が含まれているときは英語とみなす")
+(defun google-translate-enja-or-jaen (&optional string)
+  "regionか現在位置の単語を翻訳する。C-u付きでquery指定も可能"
+  (interactive)
+  (setq string
+        (cond ((stringp string) string)
+              (current-prefix-arg
+               (read-string "Google Translate: "))
+              ((use-region-p)
+               (buffer-substring (region-beginning) (region-end)))
+              (t
+               (thing-at-point 'word))))
+  (let* ((asciip (string-match
+                  (format "\\`[%s]+\\'" google-translate-english-chars)
+                  string)))
+    (run-at-time 0.1 nil 'deactivate-mark)
+    (google-translate-translate
+     (if asciip "en" "ja")
+     (if asciip "ja" "en")
+     string)))
+
+(push '("\*Google Translate\*" :height 0.5 :stick t) popwin:special-display-config)
+
+(global-unset-key (kbd "C-M-t"))
+(global-set-key (kbd "C-M-t") 'google-translate-enja-or-jaen)

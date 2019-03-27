ab557b1f2a76dddd018ef3a6bf82592e3ce5efdf
tomi <tomiacannondale@gmail.com>
2019-03-28 00:09:37 +0900

    Change magit-repository-directories to cons-cells
    https://github.com/magit/magit/commit/57ae25be9987f321475e457f59ead467feb0a4fb

--- conf/80_magit.el
+++ conf/80_magit.el
@@ -1,7 +1,13 @@
 (require 'magit)
 
 ;; `M-x magit-status` で選択出来るディレクトリ
-(setq magit-repository-directories '("~/.emacs.d" "~/.zsh.d" "~/code"))
+(setq magit-repository-directories
+      '(
+        ("~/.emacs.d" . 0)
+        ("~/.zsh.d" . 0)
+        ("~/code" . 1)
+        )
+      )
 
 ;; magit-status
 (define-key global-map "\C-xvd" 'magit-status)

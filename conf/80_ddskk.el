d6da4e402b58b7d57689dd4305207b65675b3b76
tomi <tomiacannondale@gmail.com>
2020-07-13 22:10:38 +0900

    ミニバッファに入る時imeをoffにする

--- conf/80_ddskk.el
+++ conf/80_ddskk.el
@@ -29,3 +29,14 @@
   (set-face-foreground 'skk-prefix-hiragana-face skk-cursor-hiragana-color)
   (set-face-foreground 'skk-prefix-katakana-face skk-cursor-katakana-color)
   (set-face-foreground 'skk-prefix-jisx0201-face skk-cursor-jisx0201-color))
+
+(defun my-disable-skk ()
+  "skkをoffにする。"
+  (skk-mode -1))
+
+;; 通常のミニバッファ
+(add-hook 'minibuffer-setup-hook 'my-disable-skk)
+(add-hook 'minibuffer-exit-hook 'my-disable-skk)
+;; インクリメンタル検索
+(add-hook 'isearch-mode-hook 'my-disable-skk)
+(add-hook 'isearch-mode-end-hook 'my-disable-skk)

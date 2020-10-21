b85f768424ace5f4d60397fbf80067005303a5e6
tomi <tomiacannondale@gmail.com>
2020-10-22 00:52:53 +0900

    Avoid `Invalid face reference: paren-face`

--- conf/80_mic-paren.el
+++ conf/80_mic-paren.el
@@ -1,6 +1,4 @@
 (require 'mic-paren)
 (paren-activate)
-(setq paren-match-face '(underline paren-face))
-;; boldにしたい時はこっちのコメントを外す
-;; (setq paren-match-face 'bold paren-sexp-mode t)
+(setq paren-match-face 'underline)
 (setq paren-sexp-mode t)

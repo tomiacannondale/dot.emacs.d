c0e80c654559507fbb92571fb1c21de8371253e2
tomi <tomiacannondale@gmail.com>
2021-07-15 01:06:11 +0900

    Split ddskk setting files
    org-modeでddskkが動かなくなった。

--- conf/80_ddskk.el
+++ conf/80_ddskk.el
@@ -1,3 +1,9 @@
+;;; 80_ddskk --- ddskk setting file
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'skk-autoloads)
 
 (setq default-input-method "japanese-skk")
@@ -5,35 +11,8 @@
 ;;; migemoの時に英数にする
 (setq skk-isearch-start-mode 'latin)
 
-;;; 送りがなをちゃんと指定しなくても変換される
-(setq skk-auto-okuri-process t)
+(setq skk-user-directory (expand-file-name "~/.emacs.d/conf/ddskk"))
 
-;; 辞書登録のとき、余計な送り仮名を送らないようにする
-(setq skk-check-okurigana-on-touroku 'auto)
-
-;; 変換の学習
-(require 'skk-study)
-
-;; 個人辞書
-(setq skk-jisyo (expand-file-name "~/Dropbox/skk-dictionaries/skk-jisyo.utf8"))
-(setq skk-jisyo-code 'utf-8)
-
-;; ポート1178は使用済み
-(setq skk-server-host "localhost")
-(setq skk-server-portnum 11178)
-
-(when skk-use-color-cursor
-  ;; カーソル色を変えてみる
-  (setq skk-cursor-hiragana-color       "blue"
-	   skk-cursor-katakana-color       "green"
-	   skk-cursor-abbrev-color         "red"
-	   skk-cursor-jisx0208-latin-color "red"
-	   skk-cursor-jisx0201-color       "purple"
-	   skk-cursor-latin-color          "cyan")
-  ;;
-  (set-face-foreground 'skk-prefix-hiragana-face skk-cursor-hiragana-color)
-  (set-face-foreground 'skk-prefix-katakana-face skk-cursor-katakana-color)
-  (set-face-foreground 'skk-prefix-jisx0201-face skk-cursor-jisx0201-color))
 
 (defun my-disable-skk ()
   "skkをoffにする。"
@@ -45,3 +24,5 @@
 ;; インクリメンタル検索
 (add-hook 'isearch-mode-hook 'my-disable-skk)
 (add-hook 'isearch-mode-end-hook 'my-disable-skk)
+
+;;; 80_ddskk ends here

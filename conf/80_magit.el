2b53725adf10414523fab1c1d248e8bb07bb1c57
tomi <tomiacannondale@gmail.com>
2021-12-05 01:36:04 +0900

    Clear up git-commit-mode
    - write by markdown
    - not use auto-fill

--- conf/80_magit.el
+++ conf/80_magit.el
@@ -18,5 +18,15 @@
 ;; magit-status
 (define-key global-map "\C-xvd" 'magit-status)
 
+(custom-set-variables
+ '(git-commit-major-mode 'gfm-mode)
+ ;; git-commitでauto-fillを無効にだけしたい
+ '(git-commit-setup-hook '(git-commit-save-message
+                           git-commit-setup-changelog-support
+                           git-commit-propertize-diff
+                           bug-reference-mode
+                           with-editor-usage-message))
+ )
+
 (provide '80_magit)
 ;;; 80_magit.el ends here

8e6918737f28f11d037fb7e92a9210d973f76c73
tomi <tomiacannondale@gmail.com>
2023-07-07 14:48:32 +0900

    Fix path of cmigemo
    Keep up with homebrew changes

--- conf/cocoa-emacs-migemo.el
+++ conf/cocoa-emacs-migemo.el
@@ -4,6 +4,6 @@
 ;; http://0xcc.net/migemo/  (migemoホーム)
 ;; https://gist.github.com/457761 (migemo.el公開gist)
 ;;========================================================
-(setq migemo-command "/usr/local/bin/cmigemo")
+(setq migemo-command "/opt/homebrew/bin/cmigemo")
 (setq migemo-options '("-q" "--emacs"))
-(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-dictionary "/opt/homebrew/share/migemo/utf-8/migemo-dict")
